f3f3585eee22e847008a7144cc4196387a35b09a
Clement Owireku-Bogyah <clement.owireku-bogyah@amalitech.com>
2024-10-15 13:15:01 +0000

    chore: transaction

--- custom-service/src/main/kotlin/org/clement/customservice/service/CustomerServiceImp.kt
+++ custom-service/src/main/kotlin/org/clement/customservice/service/CustomerServiceImp.kt
@@ -4,8 +4,10 @@ import org.clement.customservice.dto.CreateCustomRequest
 import org.clement.customservice.model.Customer
 import org.clement.customservice.repository.CustomRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class CustomerServiceImp(private val repository: CustomRepository) : CustomerService {
     override fun createCustomer(request: CreateCustomRequest): Customer {
         val customer: Customer = Customer(
